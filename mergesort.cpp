ad721785d18625edb80ceb25ae0fd33816e7d833
Srutarshi Ghosh <srutarshighosh06@gmail.com>
2022-04-04 00:27:17 +0530

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -66,7 +66,7 @@ void merge(int arr[], int l, int m, int r){
 
 void mergesort(int arr[], int l, int r){
 	if(l<r){
-	        int m=(l+r)/2;
+	    int m=(l+r)/2;
 		mergesort(arr, l, m);
 		mergesort(arr, m+1, r);
 		merge(arr, l, m, r);
